48938fcf6e075c48d4d79e06252ab68926c50bf3
m-spiessens <mathias@spiessens.eu>
2016-07-28 17:36:24 +0200

    Minor fixes.

---
+++ Software/cpputest/cpputest-3.7.1/include/config.h
@@ -0,0 +1,15 @@
+/*
+ * config.h
+ *
+ *  Created on: 21 mrt. 2016
+ *      Author: spiessensm
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+
+#define CPPUTEST_USE_MEM_LEAK_DETECTION 0
+#define CPPUTEST_USE_STD_CPP_LIB 0
+
+#endif /* CONFIG_H_ */
